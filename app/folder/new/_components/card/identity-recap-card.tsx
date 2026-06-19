79d4942dead4a32c2917c4d4ff678d6cacf94fb5
ethanpyth <19ka201@esisalama.org>
2026-06-19 13:15:58 +0200

    ui: put real data on form

--- app/folder/new/_components/card/identity-recap-card.tsx
+++ app/folder/new/_components/card/identity-recap-card.tsx
@@ -1,4 +1,10 @@
+'use client'
+
+import { useRecord } from "../../_context/record-context"
+
 export default function IdentityRecapCard() {
+  const { recordData, updateRecordData } = useRecord()
+  
   return (
     <div className="bg-surface-lowest border border-outline-variant overflow-hidden">
       <div className="bg-surface-low border-b border-outline-variant px-stack-lg py-3 flex justify-between items-center">
@@ -16,19 +22,19 @@ export default function IdentityRecapCard() {
       <div className="p-stack-lg space-y-4">
         <div className="flex justify-between border-b border-surface-container py-2">
           <span className="text-on-secondary-container font-label-caps text-[10px] uppercase">Nom Complet</span>
-          <span className="font-body text-body-md font-bold">JEAN DUPONT</span>
+          <span className="font-body text-body-md font-bold">{recordData.identity.firstName} { recordData.identity.lastName }</span>
         </div>
         <div className="flex justify-between border-b border-surface-container py-2">
           <span className="text-on-secondary-container font-label-caps text-[10px] uppercase">Né le</span>
-          <span className="font-body text-body-md font-bold">14 / 05 / 1982</span>
+          <span className="font-body text-body-md font-bold">{ recordData.identity.dateOfBirth.toString() }</span>
         </div>
         <div className="flex justify-between border-b border-surface-container py-2">
           <span className="text-on-secondary-container font-label-caps text-[10px] uppercase">Lieu de naissance</span>
-          <span className="font-body text-body-md font-bold">Paris (75015)</span>
+          <span className="font-body text-body-md font-bold">{ recordData.identity.placeOfBirth } (75015)</span>
         </div>
         <div className="flex justify-between py-2">
           <span className="text-on-secondary-container font-label-caps text-[10px] uppercase">Nationalité</span>
-          <span className="font-body text-body-md font-bold">Française</span>
+          <span className="font-body text-body-md font-bold">{ recordData.identity.nationality }</span>
         </div>
       </div>
     </div>
