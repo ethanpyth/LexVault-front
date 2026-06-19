8c3e03b9edc4040f0b38658f6f8436ed121a3a56
ethanpyth <19ka201@esisalama.org>
2026-06-19 12:00:13 +0200

    fix: type issues

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,9 +1,3 @@
-import { cookies } from "next/headers";
-
 export const API_BASE_URL = process.env.NODE_ENV === 'production'
 	? process.env.NEXT_PUBLIC_API_BASE_URL
 	: process.env.NEXT_LOCALHOST;
-
-const cookieStore = await cookies();
-
-export const TOKEN = cookieStore.get("access_token")?.value;
